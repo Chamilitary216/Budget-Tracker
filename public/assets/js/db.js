60e7a4de4b9d53ad82a3c5176026c0170e161f78
Chris Curry <Chamilitary216@hotmail.com>
2021-05-15 14:34:24 -0400

    figuring out why nothing is going to server when going back online

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -1,2 +1,11 @@
+const indexedDB =
+  window.indexedDB ||
+  window.msIndexedDB ||
+  window.webkitIndexedDB ||
+  window.mozIndexedDB ||
+  window.shimIndexedDB;
+
+let db;
+const request = indexedDB.open("budget", 1);
 
 window.addEventListener("online", checkDatabase);
