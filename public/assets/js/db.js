055f8a5c96d7f11069fb896df4be4539fbd109fe
Chris Curry <Chamilitary216@hotmail.com>
2021-05-15 22:18:14 -0400

    deploying

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -8,4 +8,60 @@ const indexedDB =
 let db;
 const request = indexedDB.open("budget", 1);
 
+//onupgradeneeded is an interface is the event handler for the upgradeneeded event, triggered when a database of a bigger version number than the existing stored database is loaded.
+
+request.onupgradeneeded = (event) => {
+    event.target.result.createObjectStore("pending", {
+      keyPath: "id",
+      autoIncrement: true
+    });
+  };
+
+  //on error display error; otherwise proceed
+  request.onerror = (err) => {
+    console.log(err.message);
+  };
+  
+  request.onsuccess = (event) => {
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
+
+  // Function is clled when user does offline inquiry
+  function saveRecord(record) {
+    const transaction = db.transaction("pending", "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+  }
+
+  //When user goes back online, what was done OFFLINE will be sent to server
+
+function checkDatabase() {
+    const transaction = db.transaction("pending", "readonly");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+          fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+              Accept: "application/json, text/plain, */*",
+              "Content-Type": "application/json"
+            }
+          })
+            .then((response) => response.json())
+            .then(() => {
+              const transaction = db.transaction("pending", "readwrite");
+              const store = transaction.objectStore("pending");
+              store.clear();
+            });
+        }
+      };
+    }
+  
 window.addEventListener("online", checkDatabase);
